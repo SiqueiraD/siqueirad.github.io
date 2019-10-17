286e5a8c4b4b20c4a74d7dcd94811c8cd0a8332b
Danilo Siqueira <danilosic95@gmail.com>
2019-10-17 04:48:42 -0300

    leaflet in webpack

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
-import _ from 'lodash';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import './style.css';
-import Honeycomb from './honeycomb.png';
-import Data from './data.xml';
 
-function component() {
-    const element = document.createElement('div');
+function criarMapa() {
+    return import( /* webpackChunkName: "lodash" */ 'lodash').then(({
+        default: _
+    }) => {
+        const element = document.createElement('div');
+        //const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        element.id= 'mapid';
 
+        // Add the image to our existing div.
+        /*const myIcon = new Image();
+        myIcon.src = Honeycomb;
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Honeycomb;
+        element.appendChild(myIcon);
 
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    return element;
+        console.log(Data);*/
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+criarMapa().then(map => {
+    document.body.appendChild(map);
+    var mymap = L.map('mapid').setView([-22.90309921448796, -43.12773227691651], 15);
+
+    var popup = L.popup();
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.satellite',
+        accessToken: 'pk.eyJ1IjoiZGFuaWxvc2ljIiwiYSI6ImNrMW54OHY5cTBnOXUzbXEzazRxZzg4dXAifQ.dwMFsKzaU7RHsPKcD4aPgA'
+    }).addTo(mymap);
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    //devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Titulo MaB'
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     module: {
         rules: [{
                 test: /\.css$/,
